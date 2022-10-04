35399de06a4e80e99cd1dafa2654ba1d9189b7fc
fjperez1 <francisco.perez@iesnervion.es>
2022-10-04 11:57:09 +0200

    Tema2Boletin1

--- Tema 2 - Boletin 1/src/tema2boletin1/EjercicioPrimero.java
+++ Tema 2 - Boletin 1/src/tema2boletin1/EjercicioPrimero.java
@@ -1,10 +1,37 @@
 package tema2boletin1;
 
+import java.util.Scanner;
+
 public class EjercicioPrimero {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Declaramos las variables que vamos a utilizar
+		
+		int num, resto;
+		boolean par;
+		Scanner lectura = new Scanner (System.in); //Iniciamos el scanner
 
+		//Solicitamos un número al usuario
+		
+		System.out.println("Introduce un número para saber si es par o no");
+		num = lectura.nextInt();
+		
+		//Realizamos las operaciones necesarias
+		
+		resto = num%2;
+		if (resto==0) {
+			
+			par =true;
+			
+		} else {
+			par = false;
+		}
+		
+		//Mostramos el resultado al usuario
+		
+		System.out.println("¿El número introducido es par? " + par);
 	}
-
+		
 }
---
+++ Tema 2 - Boletin 1/src/tema2boletin1/EjercicioSegundo.java
@@ -0,0 +1,10 @@
+package tema2boletin1;
+
+public class EjercicioSegundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
